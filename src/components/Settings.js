67350e2ae884e22c6d64a441c82f5f4feb1bf93b
Armagames <88371654+Armagames-Dev@users.noreply.github.com>
2025-04-09 22:03:48 +0200

    last commit (exam)

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,15 +1,14 @@
 import React, {useState, useEffect, useContext} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import {AuthContext} from "../context/AuthContext";
 import axios from 'axios';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import '../styles/Settings.css';
-import {AuthContext} from "../context/AuthContext";
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-// Fix for Leaflet default icon path
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -17,6 +16,7 @@ L.Icon.Default.mergeOptions({
     iconUrl: markerIcon,
     shadowUrl: markerShadow,
 });
+
 function Settings() {
     const [account, setAccount] = useState({ name: '', prenom: '', email: '', phone: '', password: '' });
     const [address, setAddress] = useState({ street: '', city: '', postalCode: '', country: '' });
@@ -26,20 +26,33 @@ function Settings() {
 
     const { user, token } = useContext(AuthContext);
 
-    // Fetch user data on component mount or when user ID changes
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/users/${user.id}`, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
         })
-            .then(r => {
-                setUsers(r.data);
-            })
-            .catch(err => console.error(err));
+        .then(r => {
+            setUsers(r.data);
+        })
+        .catch(err => console.error(err));
     }, [user.id, token]);
 
-    // Update position based on address changes
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setPosition([
+                        position.coords.latitude,
+                        position.coords.longitude,
+                    ]);
+                }
+            );
+        } else {
+            setPosition([48.8566, 2.3522]);
+        }
+    }, []);
+
     useEffect(() => {
         if (address.street && address.city && address.postalCode && address.country) {
             const fullAddress = `${address.street}, ${address.city}, ${address.postalCode}, ${address.country}`;
@@ -56,7 +69,6 @@ function Settings() {
         }
     }, [address]);
 
-    // Handlers
     const handleAddressChange = (e) => {
         const { name, value } = e.target;
         setAddress(prevAddress => ({
@@ -104,9 +116,6 @@ function Settings() {
         handleSaveChanges();
         setSecurePassword(false);
         window.location.reload();
-
-
-
     };
 
     const handleDeleteAccount = () => {
@@ -415,7 +424,6 @@ function Settings() {
                 <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }} onClick={handleMapClick}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
                     <Marker position={position}>
                         <Popup>
